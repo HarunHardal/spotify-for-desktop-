751b4899c7b4e3ba2179f85b73368300b5a3c106
HarunHardal <theredwolf26@gmail.com>
2022-05-09 00:17:27 +0300

    scv

--- src/loginComponent/Login.js
+++ src/loginComponent/Login.js
@@ -1,7 +1,7 @@
 import React from "react";
 import './login.css'
 const AUTH =
-  "https://accounts.spotify.com/authorize?client_id=eb815cbe4f634fc4b5b2e4764971491b&response_type=code&redirect_uri=http://localhost:3000&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state%20user-read-recently-played%20user-follow-read";
+  "https://accounts.spotify.com/authorize?client_id=eb815cbe4f634fc4b5b2e4764971491b&response_type=code&redirect_uri=https://spotify-for-desktop.netlify.app&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state%20user-read-recently-played%20user-follow-read";
 
 export default function Login() {
   return( 
