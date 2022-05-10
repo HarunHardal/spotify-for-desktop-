375fe382708184950f6cedd3aec8551051bc476c
HarunHardal <theredwolf26@gmail.com>
2022-05-10 22:48:14 +0300

    master

--- src/player/MusicPlayer.js
+++ src/player/MusicPlayer.js
@@ -37,16 +37,8 @@ export default function MusicPlayer({
   const [duration, setDuration] = useState(0);
 
   useEffect(() => {
-    progressRef.current = setInterval(() => {
-      spotifyApi.getMyCurrentPlaybackState().then(function (data) {
-        if (data.body && data.body.is_playing) {
-
-          setProgress(data.body.progress_ms);
-          setDuration(data.body.item.duration_ms);
-        }
-      });
-    }, 1000);
-  }, [spotifyApi]);
+   
+  }, []);
 
   useEffect(() => {
     setState({ artisname: artistName, trackname: trackName });
@@ -92,6 +84,15 @@ export default function MusicPlayer({
         setCounter(counter + 1);
       }, duration / 100);
 
+      progressRef.current = setInterval(() => {
+        spotifyApi.getMyCurrentPlaybackState().then(function (data) {
+          if (data.body && data.body.is_playing) {
+            setProgress(data.body.progress_ms);
+            setDuration(data.body.item.duration_ms);
+          }
+        });
+      }, 1000);
+      
       return () => {
         clearInterval(ref.current);
       };
