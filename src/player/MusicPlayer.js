2589582318f24d4056ae6617fbeaaa2d774cbead
HarunHardal <theredwolf26@gmail.com>
2022-05-10 22:38:11 +0300

    master

--- src/player/MusicPlayer.js
+++ src/player/MusicPlayer.js
@@ -47,7 +47,7 @@ export default function MusicPlayer({
         }
       });
     }, 1000);
-  }, []);
+  }, [spotifyApi]);
 
   useEffect(() => {
     setState({ artisname: artistName, trackname: trackName });
