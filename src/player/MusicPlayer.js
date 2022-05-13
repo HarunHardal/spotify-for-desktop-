3094434d663439fe4f970a4ab9688bbec22aa4f1
HarunHardal <theredwolf26@gmail.com>
2022-05-13 22:38:53 +0300

    main

--- src/player/MusicPlayer.js
+++ src/player/MusicPlayer.js
@@ -36,7 +36,28 @@ export default function MusicPlayer({
   const [progress, setProgress] = useState(0);
   const [duration, setDuration] = useState(0);
 
-  useEffect(() => {}, [spotifyApi]);
+  useEffect(() => {
+    if (!duration) return null;
+    ref.current = window.setInterval(() => {
+      setCounter(counter + 1);
+      console.log("ASDDASD");
+    }, duration / 100);
+
+    progressRef.current = window.setInterval(() => {
+      spotifyApi.getMyCurrentPlaybackState().then(function (data) {
+        if (data.body && data.body.is_playing) {
+          setProgress(data.body.progress_ms);
+          setDuration(data.body.item.duration_ms);
+        }
+      });
+      console.log("ASD");
+    }, 1000);
+
+    return () => {
+      window.clearInterval(ref.current);
+      window.clearInterval(progressRef.current);
+    };
+  }, [spotifyApi]);
 
   useEffect(() => {
     setState({ artisname: artistName, trackname: trackName });
@@ -77,26 +98,7 @@ export default function MusicPlayer({
       seekSetting;
 
     if (isPlaying) {
-      if (!duration) return null;
-      ref.current = window.setInterval(() => {
-        setCounter(counter + 1);
-        console.log("ASDDASD");
-      }, duration / 100);
-
-      progressRef.current = window.setInterval(() => {
-        spotifyApi.getMyCurrentPlaybackState().then(function (data) {
-          if (data.body && data.body.is_playing) {
-            setProgress(data.body.progress_ms);
-            setDuration(data.body.item.duration_ms);
-          }
-        });
-        console.log("ASD");
-      }, 1000);
-
-      return () => {
-        window.clearInterval(ref.current);
-        window.clearInterval(progressRef.current);
-      };
+  
     }
 
     round.addEventListener(getMouseDown(), onMouseDown);
