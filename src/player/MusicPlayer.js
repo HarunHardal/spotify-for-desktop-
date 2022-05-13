d3a305c32ab6e7579df93e6c56b44811061c8995
HarunHardal <theredwolf26@gmail.com>
2022-05-13 21:41:28 +0300

    master

--- src/player/MusicPlayer.js
+++ src/player/MusicPlayer.js
@@ -37,8 +37,27 @@ export default function MusicPlayer({
   const [duration, setDuration] = useState(0);
 
   useEffect(() => {
-   
-  }, []);
+   //progress is here
+   if (isPlaying) {
+    if (!duration) return null;
+    ref.current = setInterval(() => {
+      setCounter(counter + 1);
+    }, duration / 100);
+
+    progressRef.current = setInterval(() => {
+      spotifyApi.getMyCurrentPlaybackState().then(function (data) {
+        if (data.body && data.body.is_playing) {
+          setProgress(data.body.progress_ms);
+          setDuration(data.body.item.duration_ms);
+        }
+      });
+    }, 1000);
+    
+    return () => {
+      clearInterval(ref.current);
+    };
+  }
+  }, [isPlaying]);
 
   useEffect(() => {
     setState({ artisname: artistName, trackname: trackName });
@@ -77,26 +96,6 @@ export default function MusicPlayer({
       getRadiansInDegrees,
       finalAngleInDegrees,
       seekSetting;
-
-    if (isPlaying) {
-      if (!duration) return null;
-      ref.current = setInterval(() => {
-        setCounter(counter + 1);
-      }, duration / 100);
-
-      progressRef.current = setInterval(() => {
-        spotifyApi.getMyCurrentPlaybackState().then(function (data) {
-          if (data.body && data.body.is_playing) {
-            setProgress(data.body.progress_ms);
-            setDuration(data.body.item.duration_ms);
-          }
-        });
-      }, 1000);
-      
-      return () => {
-        clearInterval(ref.current);
-      };
-    }
 
     round.addEventListener(getMouseDown(), onMouseDown);
     document.addEventListener(getMouseUp(), onMouseUp);
