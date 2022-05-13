ee7612971ddd61b6e8c2bb6de9f5c79b8de971a1
HarunHardal <theredwolf26@gmail.com>
2022-05-13 22:53:09 +0300

    master

--- src/player/MusicPlayer.js
+++ src/player/MusicPlayer.js
@@ -86,20 +86,41 @@ export default function MusicPlayer({
       finalAngleInDegrees,
       seekSetting;
 
+   // if (isPlaying) {
+   //   if (!duration1) return null;
+   //   ref.current = setInterval(() => {
+   //     // round.setAttribute(
+   //     //   "stroke-dasharray",
+   //     //   `${(counter / 100) * 825}, 2000`
+   //     // );
+   //     setCounter(counter + 1);
+   //   }, duration1 / 100);
+   //
+   //   return () => {
+   //     clearInterval(ref.current);
+   //   };
+   // }
+
     if (isPlaying) {
       if (!duration1) return null;
       ref.current = setInterval(() => {
-        // round.setAttribute(
-        //   "stroke-dasharray",
-        //   `${(counter / 100) * 825}, 2000`
-        // );
         setCounter(counter + 1);
       }, duration1 / 100);
+
+      progressRef.current = setInterval(() => {
+        spotifyApi.getMyCurrentPlaybackState().then(function (data) {
+          if (data.body && data.body.is_playing) {
+            setProgress(data.body.progress_ms);
+            setDuration(data.body.item.duration_ms);
+          }
+        });
+      }, 1000);
 
       return () => {
         clearInterval(ref.current);
       };
     }
+
     // if(isPlaying){
     //   if (counter === 99) {
     //     next();
