6c980c39123d7c556b3c2ae04323065ff26404d0
HarunHardal <theredwolf26@gmail.com>
2022-05-16 00:10:34 +0300

    main

--- src/player/MusicPlayer.js
+++ src/player/MusicPlayer.js
@@ -46,12 +46,12 @@ export default function MusicPlayer({
 
   useInterval(() => {
     spotifyApi.getMyCurrentPlaybackState().then(function (data) {
-      if (data.body && data.body.is_playing) {
-        setProgress(data.body.progress_ms);
-        setDuration(data.body.item.duration_ms);
+      if (data?.body.is_playing) {
+        setProgress(data?.body.progress_ms);
+        setDuration(data?.body.item.duration_ms);
       }
     });
-  }, 100);
+  }, 1500);
 
  // useEffect(() => {
  //   progressRef.current = setInterval(() => {
