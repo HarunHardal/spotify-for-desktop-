d64a5989548f5aad6a6d2915f47e810989f11484
HarunHardal <theredwolf26@gmail.com>
2022-05-15 23:09:25 +0300

    main

--- src/player/MusicPlayer.js
+++ src/player/MusicPlayer.js
@@ -43,29 +43,18 @@ export default function MusicPlayer({
     setCounter(counter + 1);
   }, duration1 / 100);
 
-  useInterval(()=>{
-    console.log(isPlaying , 'ASDASDASDAS')
-    if(!isPlaying) return null
-     spotifyApi.getMyCurrentPlaybackState().then(function (data) {
-       if (data?.body.is_playing) {
-         setProgress(data.body.progress_ms);
-         setDuration(data.body.item.duration_ms);
-       }
-     });
-  },1000)
-
-
-  // useInterval(() => {
-  //   console.log(isPlaying)
-  //  if(!isPlaying) return null
-  //   spotifyApi.getMyCurrentPlaybackState().then(function (data) {
-  //     if (data?.body.is_playing) {
-  //       setProgress(data.body.progress_ms);
-  //       setDuration(data.body.item.duration_ms);
-  //     }
-  //   });
-   
-  // }, 1000);
+  useInterval(() => {
+    console.log(isPlaying)
+   if(!isPlaying) return null
+    spotifyApi.getMyCurrentPlaybackState().then(function (data) {
+      if (data.body && data.body.is_playing) {
+        console.log('in api')
+        setProgress(data.body.progress_ms);
+        setDuration(data.body.item.duration_ms);
+        console.log(ms2minutes(data.body.progress_ms))
+      }
+    });
+  }, 1000);
 
  // useEffect(() => {
  //   progressRef.current = setInterval(() => {
