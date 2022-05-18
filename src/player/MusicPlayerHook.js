6ab4da590c9ef5c197c7a277e28bcbd80d466e6c
HarunHardal <66805878+HarunHardal@users.noreply.github.com>
2022-05-19 00:37:14 +0300

    Update MusicPlayerHook.js

--- src/player/MusicPlayerHook.js
+++ src/player/MusicPlayerHook.js
@@ -10,7 +10,7 @@ export default function MusicPlayerHook() {
         spotifyApi.getMyCurrentPlaybackState().then((data)=>{
             if(data.body && data.body.is_playing){
                 setProgress(data.body.progress_ms)
-                console.log(data.body.progress_ms)
+                console.log('in hook',data.body.progress_ms)
             }
         })
     },1000)
