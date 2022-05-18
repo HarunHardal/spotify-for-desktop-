dc31ff90d5f5ed7c6e5d6f9d462868f6931c1fbb
HarunHardal <66805878+HarunHardal@users.noreply.github.com>
2022-05-19 00:41:40 +0300

    Update MusicPlayerHook.js

--- src/player/MusicPlayerHook.js
+++ src/player/MusicPlayerHook.js
@@ -1,19 +1,20 @@
-import React, { useState } from 'react'
-import useInterval from 'use-interval'
+import  { useEffect, useState } from 'react'
 import SpotifyWebApi from "spotify-web-api-node";
 const spotifyApi = new SpotifyWebApi({
     clientId: "eb815cbe4f634fc4b5b2e4764971491b",
   });
 export default function MusicPlayerHook() {
     const [progress, setProgress] = useState()
-    useInterval(()=>{
-        spotifyApi.getMyCurrentPlaybackState().then((data)=>{
-            if(data.body && data.body.is_playing){
-                setProgress(data.body.progress_ms)
-                console.log('in hook',data.body.progress_ms)
-            }
-        })
-    },1000)
-  
+    useEffect(()=>{
+        const timer = setInterval(()=>{
+            spotifyApi.getMyCurrentPlaybackState().then((data)=>{
+                if(data.body && data.body.is_playing){
+                    setProgress(data.body.progress_ms)
+                }
+            })
+        },1000)
+        return ()=> clearInterval(timer)
+    },[])
+ 
     return progress
 }
