77d1304ed15d9e277cd1d7c249e92d9c3e4dd51f
HarunHardal <66805878+HarunHardal@users.noreply.github.com>
2022-05-19 23:39:33 +0300

    main

--- src/player/MusicPlayerHook.js
+++ src/player/MusicPlayerHook.js
@@ -1,10 +1,11 @@
 import  { useEffect, useState } from 'react'
 
-export default function MusicPlayerHook(spotifyApi) {
+export default function MusicPlayerHook(spotifyApi,isPlaying) {
     const [progress, setProgress] = useState()
     useEffect(()=>{
         
         const timer = setInterval(()=>{
+            if(!isPlaying) return null
             spotifyApi.getMyCurrentPlaybackState().then((data)=>{
                 if(data.body && data.body.is_playing){
                     setProgress(data.body.progress_ms)
